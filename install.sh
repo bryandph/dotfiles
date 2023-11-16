95d8cb1b3c55f9d5d98746ab55016c022b484179
Bryan Prather-Huff <bryan@pratherhuff.com>
2023-11-15 21:57:43 -0600

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+# /usr/bin/env zsh
+
+# RUN sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh)" -- \
+zsh-in-docker.sh \
+    # -t https://github.com/denysdovhan/spaceship-prompt \
+    # -a 'SPACESHIP_PROMPT_ADD_NEWLINE="false"' \
+    # -a 'SPACESHIP_PROMPT_SEPARATE_LINE="false"' \
+    -p git \
+    -p asdf \
+    -p direnv \
+    -p https://github.com/zsh-users/zsh-autosuggestions \
+    -p https://github.com/zsh-users/zsh-completions \
+    -p https://github.com/zsh-users/zsh-history-substring-search \
+    -p https://github.com/zsh-users/zsh-syntax-highlighting \
+    -p 'history-substring-search' \
+    -a 'bindkey "\$terminfo[kcuu1]" history-substring-search-up' \
+    -a 'bindkey "\$terminfo[kcud1]" history-substring-search-down'
+
+cp .zshrc ~/
+cp .p10k.zsh ~/
