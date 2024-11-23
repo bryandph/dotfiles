3f862e260c520b5c68f6c01fe90939cdac48c839
Bryan Prather-Huff <bryan@pratherhuff.com>
2024-11-22 20:40:41 -0600

    force starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,9 +92,8 @@ function ensure_direnv_plugin() {
 }
 
 function ensure_all_asdf_plugins() {
-  . /home/$USERNAME/.asdf/asdf.sh
   echo "Ensuring all .tool-versions plugins are added to asdf..."
-  cut -d' ' -f1 .tool-versions | xargs -I{} asdf plugin add {}
+  cut -d' ' -f1 .tool-versions | xargs -I{} . /home/$USERNAME/.asdf/asdf.sh plugin add {}
 }
 
 function doIt() {
